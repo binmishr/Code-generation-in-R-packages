10fcd1020c8d2f5fa07ca0b7e5ec9c27745e7ddb
BINAYAKA MISHRA <mishra.binayaka.18005@gmail.com>
2021-10-09 10:35:50 +0530

    Add files via upload

---
+++ build.R
@@ -0,0 +1,15 @@
+library("vlmetabuildr")
+
+schema_file <- Sys.glob(file.path(system.file("schema",'vega-lite', package = 'vegawidget'),"*.json"))
+VL_SCHEMA <- jsonlite::read_json(schema_file)
+
+new_schema_path <- file.path(rprojroot::find_package_root_file(), "inst","schema", basename(schema_file))
+mini_json = jsonlite:::minify(readr::read_file(schema_file))
+cat(mini_json, file = new_schema_path)
+
+r_api <- create_api(VL_SCHEMA)
+
+r_file_path <- file.path(rprojroot::find_package_root_file(), "R","zzz_autogen_api.R")
+cat(r_api, file = r_file_path, sep = "\n")
+devtools::document()
+devtools::install()
